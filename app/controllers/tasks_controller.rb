f36554d819daa05658d7a40e1ce83400153004a8
yanagisawa-253 <anaguma253@gmail.com>
2021-09-30 13:22:15 +0000

    [update]task/index一覧表示修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,10 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
   def edit
     @task = Task.find(params[:id])
   end
@@ -18,17 +22,23 @@ class TasksController < ApplicationController
     flash[:notice] = 'タスクが投稿されました'
     redirect_to tasks_path
   end
-  
+
   def update
     @task = Task.find(params[:id])
     if  @task.update(task_params)
       redirect_to tasks_path
-    else 
+    else
       render 'index'
     end
+  end
 
   def destroy
     @task = Task.find(params[:id])
     redirect_to task_path(@task)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :body, :status)
   end
 end
