1638cb2612c4dfbb3d2323dcabc4e35ee450399a
yanagisawa-253 <anaguma253@gmail.com>
2021-10-09 10:48:42 +0000

    [update]お問合せ機能

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 
 gem 'kaminari','~> 1.2.1'
+
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'inquiry/index'
+  get 'inquiry/confirm'
+  get 'inquiry/thanks'
   devise_for :users
   root 'homes#top'
   get '/about' => 'homes#about'
@@ -16,6 +19,10 @@ Rails.application.routes.draw do
         delete 'destroy_all'
     end
   end
+  # お問合せ
+  get   'inquiry'         => 'inquiry#index'     # 入力画面
+  post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
 
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
