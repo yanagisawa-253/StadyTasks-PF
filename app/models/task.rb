ef346e60798ff1eb08d7330ddd71d2fc227f9a71
yanagisawa-253 <anaguma253@gmail.com>
2021-10-09 02:19:29 +0000

    [update]通知レコード

--- app/models/task.rb
+++ app/models/task.rb
@@ -26,12 +26,26 @@ class Task < ApplicationRecord
     end
   end
 
-  def create_notification_by(current_user)
+
+  def create_notification_comment!(current_user, post_comment_id)
+    # （ログイン中の会員と投稿者以外で）同じ投稿にコメントしているユーザーに通知を送る
+    temp_ids = Comment.where(task_id: id).where.not('user_id=? or user_id=?', current_user.id, user_id).select(:user_id).distinct
+    # 取得したユーザーへの通知を作成
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    # 投稿者へ通知を作成
+    save_notification_comment!(current_user, comment_id, user_id)
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
     notification = current_user.active_notifications.new(
       task_id: id,
-      visited_id: user_id,
-      action: 'comment'
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'Comment'
     )
+    # 自分が自分の投稿にコメントした時は通知済の扱いをする
     if notification.visitor_id == notification.visited_id
       notification.checked = true
     end
