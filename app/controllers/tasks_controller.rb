a7d33437e093a9dfbd372848fbdefdc04a9e0075
yanagisawa-253 <anaguma253@gmail.com>
2021-10-09 05:30:40 +0000

    [update]レイアウト

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,7 +35,7 @@ class TasksController < ApplicationController
     if  @task.update(task_params)
       redirect_to tasks_path
     else
-      render 'index'
+      render 'edit'
     end
   end
 
