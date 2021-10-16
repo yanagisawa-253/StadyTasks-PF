07a419cca713a8b903a2008b128755b9897949e5
yanagisawa-253 <anaguma253@gmail.com>
2021-10-16 08:23:24 +0000

    [update]お問合せ修正

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -9,10 +9,10 @@ class InquiryController < ApplicationController
     # 入力値のチェック
     @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))
     if @inquiry.valid?
-      # OK。確認画面を表示
+      # OK確認画面を表示
       render :action => 'confirm'
     else
-      # NG。入力画面を再表示
+      #入力画面を再表示
       render :action => 'index'
     end
   end
