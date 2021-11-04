56940234d210a75ab489c98452ff4d4ebc140774
yanagisawa-253 <anaguma253@gmail.com>
2021-11-04 11:57:08 +0000

    [update]inquiry_specリダイレクト確認

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # お問合せ
   get   'inquiry'         => 'inquiry#index'     # 入力画面
   post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
-  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    
 
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
--- spec/system/inquiy_spec.rb
+++ spec/system/inquiy_spec.rb
@@ -8,9 +8,9 @@ RSpec.configure do |config|
   end
   describe 'お問合せ機能' do
     before do
-        visit inquiry_path
-      end
-    describe '一覧画面について' do
+      visit inquiry_path
+    end
+    describe '入力画面について' do
       context '表示の確認' do
         it 'URLがあっているか' do
           expect(current_path).to eq '/inquiry'
@@ -29,6 +29,16 @@ RSpec.configure do |config|
         end
         it '確認ボタンがあるか' do
           expect(page).to have_button '確認'
+        end
+      end
+
+      context 'フォームの確認' do
+        it '確認ボタンが押せるか' do
+          fill_in 'inquiry[name]', with: Faker::Lorem.characters(number:5)
+          fill_in 'inquiry[email]', with: Faker::Internet.email
+          fill_in 'inquiry[message]', with: Faker::Lorem.characters(number:20)
+          click_button '確認'
+          expect(page).to have_current_path inquiry_confirm_path
         end
       end
     end
